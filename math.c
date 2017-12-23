8ab7c26e3e6f06f63b9afea3b633928766690a7e
s7rul <serraerik1@gmail.com>
2017-12-23 19:59:49 +0100

    removed unused math functions

--- math.c
+++ math.c
@@ -2,22 +2,6 @@
 
 #include <stdio.h>
 
-int add(int x, int y){
-	return x + y;
-}
-
-int sub(int x, int y){
-	return x - y;
-}
-
-int mult(int x, int y){
-	return x * y;
-}
-
-int div(int x, int y){
-	return x / y;
-}
-
 int mypow(int x, int y){
 	int sum = 0;
 
