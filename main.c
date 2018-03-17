e1c69ef1eeb244c4692ee104e09b00e71d6ca266
s7rul <serraerik1@gmail.com>
2018-03-17 16:12:54 +0100

    solved it with a singel stack solution

--- main.c
+++ main.c
@@ -47,6 +47,15 @@ int char_to_int(char charnumber){
 	}
 	return result;
 }
+
+int is_operator(char charoperator){
+
+	if (charoperator == '+' || charoperator == '-' || charoperator == '*' || charoperator == '/'){
+		return 1;
+	}
+	return 0;
+}
+
 int leght_of_number(char *charnumber){
 
 	char scanner = ' ';
@@ -75,35 +84,7 @@ int string_to_int(char *charnumber){
 	return output;
 }
 
-int operand_to_int(char operand){
-	//returns
-	// error = 0
-	// + = 1
-	// * = 2
-	// - = 3
-	// / = 4
-
-	int result = 0;
-
-	switch (operand){
-		case '+':
-			result = 1;
-			break;
-		case 'x':
-			result = 2;
-			break;
-		case '-':
-			result = 3;
-			break;
-		case '/':
-			result = 4;
-			break;
-	}
-
-	return result;
-}
-
-int is_number(char* input){//returns 1 for numbers and 0 for non numbers
+int is_number(char *input){//returns 1 for numbers and 0 for non numbers
 
 	int n = 0;
 
@@ -129,20 +110,6 @@ int count_numbers(char *argv[], int argc){
 	return count;
 }
 
-int count_operands(char *argv[], int argc){
-	//count the amount of numbers
-	
-	int count = 0;
-
-	for (int n = 1; n < argc; n++){
-		if (operand_to_int(argv[n][0]) != 0){
-			count++;
-		}
-	}
-
-	return count;
-}
-
 void printhelp(){
 	printf("############## Help for rpn ##################\n");
 	printf("\n");
@@ -154,60 +121,42 @@ void printhelp(){
 	return;
 }
 
-void fill_numberstack(stackT *stack, int argc, char *argv[]){
-	//fill numberstack with numbers so the fist number is at the bottom
-	
-	for (int n = 1; n < argc; n++){
-		if (is_number(argv[n])){
-			StackPush(stack, string_to_int(argv[n]));
-		}
-	}
-}
-
-void fill_operatorstack(stackT *stack, int argc, char *argv[]){
-	
-	for (int n = argc - 1; n > 0; n--){
-		if (is_number(argv[n]) == 0){
-			StackPush(stack, operand_to_int(argv[n][0]));
-		}
-	}
-}
-
-int prossesing(stackT *numberstack, stackT *operatorstack){
+int prossesing(stackT *stack, char *argv[], int argc){
 	//prossesing the stacks to get an answer
 	int a = 0;
 	int b = 0;
 
-	for (int n = 0; n < operatorstack->maxSize; n++){
-		a = StackPop(numberstack);
-		b = StackPop(numberstack);
+	for (int n = 0; n < argc; n++){
+		if (is_number(argv[n])){
+			StackPush(stack, string_to_int(argv[n]));
+		}
+		if (is_operator(argv[n][0])){
+			a = StackPop(stack);
+			b = StackPop(stack);
 
-		switch (StackPop(operatorstack)){
-			case 1:
-				printf("added\n");
-				StackPush(numberstack, a + b);
-				break;
-			case 2:
-				printf("mult\n");
-				StackPush(numberstack, a * b);
-				break;
-			case 3:
-				printf("sub\n");
-				StackPush(numberstack, b - a);
-				break;
-			case 4:
-				printf("div\n");
-				StackPush(numberstack, b / a);
-				break;
+			switch(argv[n][0]){
+				case '+':
+					StackPush(stack, b + a);
+					break;
+				case '-':
+					StackPush(stack, b - a);
+					break;
+				case '*':
+					StackPush(stack, b * a);
+					break;
+				case '/':
+					StackPush(stack, b / a);
+					break;
+			}
 		}
 	}
-	return StackPop(numberstack);
+
+	return StackPop(stack);
 }
 
 int main( int argc, char *argv[]){
 
-	stackT numberstack;
-	stackT operatorstack;
+	stackT stack;
 
 	int result;
 
@@ -216,13 +165,9 @@ int main( int argc, char *argv[]){
 	}
 
 	else {
-		StackInit(&numberstack, count_numbers(argv, argc));
-		StackInit(&operatorstack, count_operands(argv, argc));
-		fill_numberstack(&numberstack, argc, argv);
-		fill_operatorstack(&operatorstack, argc, argv);
-		result = prossesing(&numberstack, &operatorstack);
-		StackDestroy(&numberstack);
-		StackDestroy(&operatorstack);
+		StackInit(&stack, count_numbers(argv, argc));
+		result = prossesing(&stack, argv, argc);
+		StackDestroy(&stack);
 	}
 
 	printf("%d\n", result);
